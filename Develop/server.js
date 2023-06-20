a68b8440dae3ca79e730257ac374957450ba0ae7
Alex Marquez <marquez798@gmail.com>
2023-06-20 18:26:58 -0500

    created function to be able to write to file

--- Develop/server.js
+++ Develop/server.js
@@ -30,6 +30,24 @@ app.get('/api/notes', (req, response) => {
   readFromFile('./db/db.json').then((data) => response.json(JSON.parse(data)));
 });
 
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend=(content, file) =>{
+  fs.readFile(file, "utf-8", (err ,data)=>{
+    if (err) {
+      console.error(err)
+    } else {
+      const parsedData = JSON.parse(data)
+      parsedData.push(content)
+      writeToFile(file, parsedData)
+    }
+  })
+
+};
+
 
 
 app.post('/api/notes', (req, res) => {
