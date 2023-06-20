e1e91e385d1c671234a2a38d1046fefb93c7e9e7
Alex Marquez <marquez798@gmail.com>
2023-06-20 01:34:57 -0500

    added get route

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,29 @@ const { error } = require('console');
 const PORT = 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+const readFromFile = util.promisify(fs.readFile);
+
+
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get('/api/notes', (req, response) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => response.json(JSON.parse(data)));
+});
+
+
 
 
 app.listen(PORT, () =>
